47ad2bb594e1972d01dc69619f0bb0f508b028bb
Elo Mukoro <Elomukoro@gmail.com>
2023-08-14 16:54:03 -0500

    first commit

---
+++ Nota-Link/hardhat.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  solidity: "0.8.0", // Specify the Solidity version you're using
+  paths: {
+    sources: "./contracts", // Path to your contract source files
+  },
+  networks: {
+    // Define your networks here (e.g., localhost, Rinkeby, etc.)
+  },
+};
