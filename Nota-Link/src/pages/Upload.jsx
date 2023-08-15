03e20cacde689139d5ff5d1be447fa74d398763a
mobmatt <matthewowusu29@gmail.com>
2023-08-15 16:54:55 -0500

    upload modal updated

--- Nota-Link/src/pages/Upload.jsx
+++ Nota-Link/src/pages/Upload.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { useToast } from '@chakra-ui/react';
-import "../styles/Upload.css";
+import { useToast, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure } from '@chakra-ui/react';
+import '../styles/Upload.css';
 
 const Upload = ({ connectWallet }) => {
   const [selectedFiles, setSelectedFiles] = useState([]);
-  const [connectedWalletAddress, setConnectedWalletAddress] = useState(null); // New state variable
+  const [connectedWalletAddress, setConnectedWalletAddress] = useState(null);
+  const [isSigning, setIsSigning] = useState(false); // State for showing the "Sign" button
   const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleFileChange = (e) => {
     const files = Array.from(e.target.files);
@@ -16,6 +18,7 @@ const Upload = ({ connectWallet }) => {
     e.preventDefault();
     const files = Array.from(e.dataTransfer.files);
     setSelectedFiles(files);
+    setIsSigning(true); // Show the "Sign" button when files are dropped
   };
 
   const handleDragOver = (e) => {
@@ -23,20 +26,26 @@ const Upload = ({ connectWallet }) => {
   };
 
   const handleUpload = () => {
-    // Handle uploading selectedFiles here
+    setIsSigning(true); // Show the "Sign" button when files are uploaded
+    onOpen(); // Open the modal
+  };
+
+  const handleSign = () => {
+    // Handle signing the files and showing a success message
+    setIsSigning(false); // Reset the signing state
     toast({
-      title: 'Files uploaded',
-      description: `${selectedFiles.length} files uploaded successfully.`,
+      title: 'Files signed and uploaded',
+      description: `${selectedFiles.length} files signed and uploaded successfully.`,
       status: 'success',
       duration: 3000,
       isClosable: true,
     });
     setSelectedFiles([]);
+    onClose(); // Close the modal
   };
 
   const handleWalletConnect = async () => {
     try {
-      // Call the connectWallet function and store the wallet address
       const walletAddress = await connectWallet();
       setConnectedWalletAddress(walletAddress);
     } catch (error) {
@@ -77,6 +86,38 @@ const Upload = ({ connectWallet }) => {
           </button>
         )}
       </div>
+
+      {/* Modal */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Notarize Files</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {isSigning ? (
+              <p>Click sign to notarize selected files</p>
+            ) : (
+              <p>Drag and drop files or use the upload button.</p>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            {isSigning ? (
+              <>
+                <Button colorScheme="blue" mr={3} onClick={handleSign}>
+                  Sign
+                </Button>
+                <Button colorScheme="gray" onClick={onClose}>
+                  Close
+                </Button>
+              </>
+            ) : (
+              <Button colorScheme="blue" onClick={onClose}>
+                Close
+              </Button>
+            )}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
