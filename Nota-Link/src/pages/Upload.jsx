73c44d1c59bcac78d10fa204798d0bae19192891
mobmatt <matthewowusu29@gmail.com>
2023-08-15 14:58:46 -0500

    modal added to upload widget

--- Nota-Link/src/pages/Upload.jsx
+++ Nota-Link/src/pages/Upload.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
-import { useToast } from '@chakra-ui/react';
+import { useToast, useDisclosure } from '@chakra-ui/react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Spinner, // Import the Spinner component
+} from '@chakra-ui/react';
 
 const Upload = () => {
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const [uploading, setUploading] = useState(false); // State to track uploading state
   const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleFileChange = (e) => {
     const files = Array.from(e.target.files);
@@ -21,15 +34,23 @@ const Upload = () => {
   };
 
   const handleUpload = () => {
-    // Handle uploading selectedFiles here
-    toast({
-      title: 'Files uploaded',
-      description: `${selectedFiles.length} files uploaded successfully.`,
-      status: 'success',
-      duration: 3000,
-      isClosable: true,
-    });
-    setSelectedFiles([]);
+    // Simulate uploading process
+    setUploading(true);
+
+    setTimeout(() => {
+      // Handle uploading selectedFiles here
+      toast({
+        title: 'Files uploaded',
+        description: `${selectedFiles.length} files uploaded successfully.`,
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      });
+
+      setUploading(false);
+      setSelectedFiles([]);
+      onClose(); // Close the modal after uploading
+    }, 2000); // Simulated upload time
   };
 
   return (
@@ -46,12 +67,42 @@ const Upload = () => {
       >
         Drag and drop files here
       </div>
-      <button onClick={handleUpload}>Upload</button>
-      <div>
-        {selectedFiles.map((file, index) => (
-          <div key={index}>{file.name}</div>
-        ))}
-      </div>
+      <Button onClick={onOpen}>Upload</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Notarize File</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {uploading ? (
+              <div style={{ textAlign: 'center' }}>
+                <Spinner size="xl" />
+                <p>Uploading...</p>
+              </div>
+            ) : (
+              // Your modal content here
+              selectedFiles.map((file, index) => (
+                <div key={index}>{file.name}</div>
+              ))
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={handleUpload}
+              disabled={uploading} // Disable the button while uploading
+            >
+              Sign
+            </Button>
+            <Button variant="ghost" onClick={onClose} disabled={uploading}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
