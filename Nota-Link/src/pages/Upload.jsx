3a3f3004ff27e502473b0d60fac6c3a3e324838c
Elo Mukoro <Elomukoro@gmail.com>
2023-08-21 13:30:20 -0500

    Added symmetrickey

--- Nota-Link/src/pages/Upload.jsx
+++ Nota-Link/src/pages/Upload.jsx
@@ -1,17 +1,38 @@
 import React, { useState } from 'react';
 import { useToast, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure } from '@chakra-ui/react';
 import '../styles/Upload.css';
-import axios from 'axios';
+import { Web3Modal } from '@web3modal/react';
+import { useWeb3Modal } from '@web3modal/react';
+import { create } from 'ipfs-http-client';
+import CryptoJS from 'crypto-js';
+import { useAccount } from 'wagmi';
+
+
+
 
 
 
 
 
 const Upload = ({ Web3Button }) => {
+  const { address, isConnecting, isDisconnected } = useAccount()
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const [publicKey, setPublicKey] = useState('');
   const [isSigning, setIsSigning] = useState(false); // State for showing the "Sign" button
   const toast = useToast();
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+
+ 
+
+  const ipfs = create({
+    host: 'ipfs.infura.io',
+    port: '5001',
+    protocol: 'https',
+    headers: {
+      authorization: 'Basic ' + btoa('2U7lmm7vA3drtU7B5lgLM46BB2m:45da224ddbdf444a7ba88415425a28ce')
+    }
+  });
 
   const handleFileChange = (e) => {
     const files = Array.from(e.target.files);
@@ -30,44 +51,76 @@ const Upload = ({ Web3Button }) => {
   };
 
   const handleUpload = () => {
-    console.log('Modal should open');
     setIsSigning(true); // Show the "Sign" button when files are uploaded
     onOpen(); // Open the modal
   };
 
-  const handleSign = async () => {
-    setIsSigning(false);
-  
-    const formData = new FormData();
-    selectedFiles.forEach(file => {
-      formData.append('file', file);
-    });
-    try {
-      // Send files to server and get encryption key in response
-      const response = await axios.post('http://localhost:3000/upload', formData);
 
-      const { key: encryptionKey } = response.data;
-  
-      // Handle success message and update state
+
+  const handleSign =  async () => {
+
+    
+    
+    // Handle signing the files and showing a success message
+    setIsSigning(false); // Reset the signing state
+    try {
+      // Upload files to IPFS
+      const uploadedFiles = await Promise.all(selectedFiles.map(async (file) => {
+        const fileBuffer = await file.arrayBuffer();
+        const ipfsResponse = await ipfs.add(fileBuffer);
+        return ipfsResponse.path;
+      }));
+      
+      console.log('Uploaded Files:', uploadedFiles);
+      
+      const symmetricKey = CryptoJS.lib.WordArray.random(32); // 256-bit key
+
+      // Encrypt each CID using the symmetric key
+      const encryptedCIDs = uploadedFiles.map((cid) => {
+        const encryptedCID = encryptString(cid, symmetricKey);
+        return encryptedCID;
+      });
+      
+      console.log('Encrypted CIDs:', encryptedCIDs);
+
+      // Show success message
       toast({
-        title: 'Files signed and uploaded',
-        description: `${selectedFiles.length} files signed and uploaded successfully.`,
+        title: 'CIDs encrypted and uploaded to IPFS',
+        description: `${uploadedFiles.length} CIDs encrypted and uploaded successfully.`,
         status: 'success',
         duration: 3000,
         isClosable: true,
       });
+
+      
+ 
+
   
-      // Save the encryption key to MongoDB
-      const saveKeyResponse = await axios.post('/save-key', { key: encryptionKey });
-      console.log('Encryption key saved:', saveKeyResponse.data);
-  
+      // Clear selected files and close modal
       setSelectedFiles([]);
       onClose(); // Close the modal
     } catch (error) {
-      // Handle error
-      console.error('Error signing and uploading files:', error);
+      console.error('Error uploading to IPFS:', error);
+      toast({
+        title: 'Error',
+        description: 'An error occurred while uploading files to IPFS.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
     }
+  }
+   
+
+  const encryptString = (data, key) => {
+    const iv = CryptoJS.lib.WordArray.random(16); // Initialization vector for encryption
+    const encryptedData = CryptoJS.AES.encrypt(data, key, { iv });
+    return { encryptedData: encryptedData.toString(), iv: iv.toString() };
   };
+
+
+  
+
  
 
   return (
@@ -96,6 +149,7 @@ const Upload = ({ Web3Button }) => {
       <div>
         <Web3Button />
       </div>
+      <div>{address}</div>
      
 
       {/* Modal */}
@@ -134,3 +188,4 @@ const Upload = ({ Web3Button }) => {
 };
 
 export default Upload;
+
